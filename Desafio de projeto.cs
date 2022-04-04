5a8893af0071b37eed456b119473693d9165304d
CarvalhoLayla <101757865+CarvalhoLayla@users.noreply.github.com>
2022-04-04 12:02:32 -0300

    Add files via upload

---
+++ Desafio de projeto.cs
@@ -0,0 +1,17 @@
+namespace Desafio_de_Projeto.Entiters
+{
+    public class Ninja : Hero
+
+    {
+          public Ninja ( String Name, int Level, String HeroType){ 
+          This Name = Name;
+        this Level = Level;
+        This HeroType = HeroType;
+
+       }
+       
+Console.WriteLine (Arus.Attack(1));
+Console.WriteLine ( Wizard.Attack(7));
+        
+    }
+}
